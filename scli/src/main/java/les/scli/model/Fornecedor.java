a219045f12e0a19563cfde9afd155f8b4f490e7d
Arthur Hülle Leite <arthurleitee@gmail.com>
2022-05-21 01:51:00 -0300

    Model Fornecedor

---
+++ scli/src/main/java/les/scli/model/Fornecedor.java
@@ -0,0 +1,34 @@
+package les.scli.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Fornecedor implements Serializable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column()
+  private String nome;
+
+  @Column()
+  private String cpf;
+
+  @Column()
+  private String uf;
+
+  @Column()
+  private String cidade;
+
+  @Column()
+  private String bairro;
+
+  @Column()
+  private String rua;
+}
