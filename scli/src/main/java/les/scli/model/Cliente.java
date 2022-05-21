117000fb8833756c438b1822155f47bad27b7bba
IPTV PlayOn <jonatanbandeira21@gmail.com>
2022-05-21 01:44:28 -0300

    add model cliente

---
+++ scli/src/main/java/les/scli/model/Cliente.java
@@ -0,0 +1,45 @@
+package les.scli.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = { "id" })
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column()
+	private String nome;
+
+	@Column()
+	private String cpf;
+
+	@Column()
+	private String email;
+
+	@Column()
+	private String senha;
+
+	@Column()
+	private String uf;
+
+	@Column()
+	private String cidade;
+
+	@Column()
+	private String bairro;
+
+	@Column()
+	private String rua;
+
+}
