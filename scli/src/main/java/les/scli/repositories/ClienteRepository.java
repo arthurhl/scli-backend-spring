87a9199fdb56760d01e2a318e1287372f96b1924
kaiquen <kaiquenascente@gmail.com>
2022-05-21 19:03:29 -0300

    ...

--- scli/src/main/java/les/scli/repositories/ClienteRepository.java
+++ scli/src/main/java/les/scli/repositories/ClienteRepository.java
@@ -1,8 +1,11 @@
 package les.scli.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import les.scli.model.Cliente;
+
 @Repository
-public interface ClienteRepository {
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
     
 }
