410cd8d56164047b546ca8dc2aa8cc0e8e8a2d72
jonathanbandeira <jonatanbandeira21@gmail.com>
2022-07-05 00:36:33 -0300

    modified repository

--- scli/src/main/java/les/scli/repositories/ClienteRepository.java
+++ scli/src/main/java/les/scli/repositories/ClienteRepository.java
@@ -1,11 +1,20 @@
 package les.scli.repositories;
 
+// import java.util.Collection;
+
+// import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import les.scli.model.Cliente;
 
+//Jonatan Bandeira
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-    
+    // @Transactional
+    // @Query(value = "SELECT * FROM ORCAMENTO AS ORC INNER JOIN SERVICO AS SER ON ORC.ID = SER.ORCAMENTO_ID INNER JOIN REALIZAR_SERVICO_EMPRESA AS REL_EMP ON SER.ID = REL_EMP.SERVICO_ID AND ORC.CLIENTE_ID = 1 UNION SELECT * FROM ORCAMENTO AS ORC INNER JOIN SERVICO AS SER ON ORC.ID = SER.ORCAMENTO_ID INNER JOIN REALIZAR_SERVICO_EMPRESA AS REL_EMP ON SER.ID = REL_EMP.SERVICO_ID AND ORC.CLIENTE_ID = 1", nativeQuery = true)
+    // public Collection<Cliente> findDebito();
+
 }
--- scli/src/main/java/les/scli/repositories/OrcamentoRepository.java
+++ scli/src/main/java/les/scli/repositories/OrcamentoRepository.java
@@ -1,11 +1,24 @@
 package les.scli.repositories;
 
+// import java.util.Collection;
+
+// import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 
 import les.scli.model.Orcamento;
 
 @Repository
-public interface OrcamentoRepository extends JpaRepository<Orcamento, Integer>{
+public interface OrcamentoRepository extends JpaRepository<Orcamento, Integer> {
+
+    // @Transactional
+    // public Collection<Orcamento> findByCliente();
+
+    // @Transactional
+    // @Query(value = "SELECT COUNT(ORCAMENTO.DATA_INICIO) FROM ORCAMENTO WHERE ORCAMENTO.ID < 3", nativeQuery = true)
+    // public Collection<Orcamento> findByCliente(Integer idCliente);
     
 }
